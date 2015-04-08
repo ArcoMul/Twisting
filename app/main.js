9af1b424706a73274177a23db39e0187e26303f4
Arco Mul <me@arcomul.nl>
2015-04-08 22:59:55 +0200

    Border less application + dragable toolbar + press <d> to open console

--- app/main.js
+++ app/main.js
@@ -1,15 +1,17 @@
 // Break out the application running from the configuration definition to
 // assist with testing.
 
-var fs = require('fs');
+var fs = require('fs'),
+    gui = window.require('nw.gui');
+
 require.extensions['.html'] = function (module, filename) {
     module.exports = fs.readFileSync(filename, 'utf8');
 };
 
-var app = require('./app');
-var Router = require('./router');
-var $ = require('jquery');
-var Backbone = require('backbone');
+var $ = require('jquery'),
+    Backbone = require('backbone'),
+    app = require('./app'),
+    Router = require('./router');
 
 Backbone.$ = $;
 
@@ -21,3 +23,16 @@ app.router = new Router();
 // root folder to '/' by default.  Change in app.js.
 Backbone.history.start({ root: app.root });
 app.router.navigate('/index.html', {trigger: true});
+
+// Press <d> to open dev tools
+$(window.document).on('keypress', function (e) {
+    console.log(e);
+    if (e.keyCode == 100) {
+        gui.Window.get().showDevTools();
+    }
+});
+
+// Enable shadow
+// var win = gui.Window.get();
+// win.minimize();
+// win.restore();
