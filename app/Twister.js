0a489d1c52a4c7db7dfd8a0762a0fa4d29b17c30
Arco Mul <me@arcomul.nl>
2015-04-08 23:00:43 +0200

    Cache avatars on disk

--- app/Twister.js
+++ app/Twister.js
@@ -2,6 +2,7 @@
 
 var _ = require("underscore");
 var rpc = require('json-rpc2');
+var fs = require("fs");
 
 module.exports = (function () {
 
@@ -44,13 +45,58 @@ module.exports = (function () {
             callback (err, data);
         });
     }
+
+
+    var getAvatarFromFileSystem = function (username, callback) {
+    }
     
     var getAvatar = function (username, callback)
     {
-        twisterRpc("dhtget", [username, "avatar", "s"], function (err, data) {
-            if (err) callback(err);
-            if (!data || data.length == 0 || !data[0]) return callback();
-            callback (null, data[0].p.v);
+        var filename = "app/avatars/" + username;
+
+        // TODO: don't check for both file in this spagetti code
+        fs.exists(filename + ".png", function (result) {
+            // It excists, return the path
+            if (result) {
+                filename += ".png";
+                return callback(null, filename);
+            }
+
+            fs.exists(filename + ".jpg", function (result) {
+                // It excists, return the path
+                if (result) {
+                    filename += ".jpg";
+                    return callback(null, filename);
+                }
+
+                twisterRpc("dhtget", [username, "avatar", "s"], function (err, data) {
+                    if (err) return callback(err);
+                    if (!data || data.length == 0 || !data[0]) return callback();
+
+                    var img = data[0].p.v;
+
+                    // Got an avatar, cache it on the filesystem
+                    var base64;
+                    if (img.indexOf("data:image/jpeg;base64,") != -1) {
+                        base64 = img.replace(/^data:image\/jpeg;base64,/, "");
+                        filename += ".jpg";
+                    } else if (img.indexOf("data:image/png;base64,") != -1) {
+                        base64 = img.replace(/^data:image\/png;base64,/, "");
+                        filename += ".png";
+                    } else if (img == "img/genericPerson.png") {
+                        // This is some sort of default image? Anyway, return like there is no image
+                        callback(null, null);
+                    } else {
+                        console.log('No jpg or png for', username, img);
+                    }
+                    fs.writeFile(filename, base64, 'base64', function(err) {
+                        if (err) {
+                            console.log('Error saving avatar:', err);
+                        }
+                        callback (err, filename);
+                    });
+                });
+            });
         });
     }
 
