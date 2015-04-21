6cd5d9fa96cefde8c560e83826cce034952e67b0
Arco Mul <me@arcomul.nl>
2015-04-21 20:31:56 +0200

    Minimize, maximize and close buttons

--- app/app.js
+++ app/app.js
@@ -2,12 +2,16 @@
 
 // External dependencies.
 var _ = require("underscore"),
+    gui = window.require('nw.gui'),
+    $ = require("jquery"),
     UserModel = require("./models/User"),
     MainMenuView = require("./views/MainMenu"),
     MainPageView = require("./views/MainPage");
 
 // Alias the module for easier identification.
 var app = module.exports;
+var win = gui.Window.get();
+var maximized = false;
 
 app.user = undefined;
 app.changeUser = function (user) {
@@ -22,3 +26,19 @@ app.mainView = new MainPageView();
 
 // The root path to run the application through.
 app.root = '/';
+
+$("#window-controls .close").on('click', function () {
+    win.close();    
+});
+$("#window-controls .minimize").on('click', function () {
+    win.minimize();    
+});
+$("#window-controls .scale").on('click', function () {
+    if (maximized) {
+        win.unmaximize();
+        maximized = false;
+    } else {
+        win.maximize();
+        maximized = true;
+    }
+});
