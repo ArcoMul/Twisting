040b83227afe256a228f02817c4cc447eba324cc
Arco Mul <me@arcomul.nl>
2015-10-13 19:00:04 +0200

    Display conversation when clicking on conversation preview

--- src/app/models/Conversation.js
+++ src/app/models/Conversation.js
@@ -7,7 +7,9 @@ var $ = require("jquery"),
     async = require("async"),
     Twister = require("../Twister"),
     MessagesCollection = require("../collections/messages"),
-    UserCollection = require("../collections/users");
+    UserCollection = require("../collections/users"),
+    UserModel = require("../models/User"),
+    MessageModel = require("../models/Message");
 
 Backbone.$ = $;
 
@@ -39,6 +41,32 @@ module.exports = Backbone.Model.extend({
     addMessage: function (message) {
         this.get('messages').add(message);
         this.set('lastMessage', this.get('messages').at(this.get('messages').length - 1));
+    },
+
+    fetchMessages: function (username, n, done) {
+        var self = this;
+        var aliases = [];
+        if (this.isGroupConversation()) {
+            aliases.push(this.get('alias'));
+        } else {
+            this.get('users').each(function (user) {
+                aliases.push(user.get('username'));
+            });
+        }
+        Twister.getMessages(username, n, aliases, function (err, data) {
+            if (err) return done(err);
+            _.each(data, function (messages, alias) {
+                _.each(messages, function (message) {
+                    var fromUser = self.get('users').findWhere({username: message.from});
+                    if (!fromUser) {
+                        fromUser = new UserModel({username: message.from});
+                        self.get('users').add(fromUser);
+                    }
+                    self.addMessage(new MessageModel().parse(message, fromUser));
+                });
+            });
+            done(null, self);
+        });
     }
 
 });
--- src/app/views/Messages.js
+++ src/app/views/Messages.js
@@ -6,6 +6,7 @@ var $ = require("jquery"),
     app = require("../app"),
     _ = require("underscore"),
     messagesTemplate = _.template(require("../templates/messages.html")),
+    conversationTemplate = _.template(require("../templates/conversation.html")),
     Twister = require("../Twister"),
     UserModel = require('../models/User'),
     UserCollection = require('../collections/users'),
@@ -18,10 +19,11 @@ Backbone.$ = $;
 
 module.exports = Backbone.View.extend({
 
-    messages: null,
+    users: null,
+    conversations: null,
 
     events: {
-        'click .conversation-list .conversation': 'openConversation'
+        'click .conversation-list .message': 'openConversation'
     },
 
     initialize: function(options) {
@@ -30,8 +32,8 @@ module.exports = Backbone.View.extend({
 
         this.render();
 
-        var users = new UserCollection();
-        var conversations = new ConversationCollection();
+        this.users = new UserCollection();
+        this.conversations = new ConversationCollection();
 
         var aliases = [];
 
@@ -53,14 +55,14 @@ module.exports = Backbone.View.extend({
                         var c = new ConversationModel();
                         c.setGroupAliases(group.alias);
                         _.each(group.members, function (username) {
-                            var user = users.findWhere({username: username});
+                            var user = self.users.findWhere({username: username});
                             if (!user) {
                                 user = new UserModel({username: username});
-                                users.add(user);
+                                self.users.add(user);
                             }
                             c.addUser(user);
                         });
-                        conversations.add(c);
+                        self.conversations.add(c);
                     });
                     next();
                 });
@@ -69,25 +71,25 @@ module.exports = Backbone.View.extend({
                 Twister.getMessages(app.user.get('username'), 1, aliases, function (err, data) {
                     if (err) return next(err);
                     _.each(data, function (messages, alias) {
-                        var conversation = conversations.findWhere({alias: alias});
+                        var conversation = self.conversations.findWhere({alias: alias});
                         // Direct messages don't have a conversation yet
                         // group messages do
                         if (!conversation) {
                             conversation = new ConversationModel();
-                            conversations.add(conversation);
+                            self.conversations.add(conversation);
                         }
                         if (!conversation.isGroupConversation()) {
-                            var user = users.findWhere({username: alias});
+                            var user = self.users.findWhere({username: alias});
                             if (!user) {
                                 user = new UserModel({username: alias});
-                                users.add(user);
+                                self.users.add(user);
                             }
                             conversation.addUser(user);
                         }
-                        var fromUser = users.findWhere({username: messages[0].from});
+                        var fromUser = self.users.findWhere({username: messages[0].from});
                         if (!fromUser) {
                             fromUser = new UserModel({username: alias});
-                            users.add(fromUser);
+                            self.users.add(fromUser);
                         }
                         conversation.addMessage(new MessageModel().parse(messages[0], fromUser));
                     });
@@ -96,20 +98,36 @@ module.exports = Backbone.View.extend({
             }]
         }, function (err, results) {
             if (err) return console.error('Error getting direct and group messages', err);
-            conversations.sort();
-            self.render(conversations);
+            self.conversations.sort();
+            self.render();
         });
     },
 
     openConversation: function (e) {
+        var self = this;
         var $conversation = $(e.currentTarget);
-        console.log($conversation);
+        var cid = $conversation.attr('data-cid');
+        var conversation = this.conversations.get(cid);
+        if (conversation.get('messages').length < 10) {
+            conversation.fetchMessages(app.user.get('username'), 10, function (err, conversation) {
+                if (err) return console.error('Error fetching messages for conversation');
+                self.renderConversation(conversation);
+            });
+        }
     },
 
-    render: function(conversations) {
-        this.$el.html(messagesTemplate({
-            conversations: conversations
+    renderConversation: function (conversation) {
+        this.$conversation.html(conversationTemplate({
+            conversation: conversation
         }));
+        this.$conversation.scrollTop(this.$conversation[0].scrollHeight);
+    },
+
+    render: function() {
+        this.$el.html(messagesTemplate({
+            conversations: this.conversations
+        }));
+        this.$conversation = this.$el.find('#conversation').first();
         return this;
     }
 });
